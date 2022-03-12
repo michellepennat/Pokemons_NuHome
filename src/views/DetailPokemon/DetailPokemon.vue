ccbe036fd044c7f7ba18f2712fac2ca8a71a40a1
Michelle Penna <mp.design.apps@gmail.com>
2022-03-12 15:25:11 -0500

    refactor(DetailPokemon): styles web

--- src/views/DetailPokemon/DetailPokemon.vue
+++ src/views/DetailPokemon/DetailPokemon.vue
@@ -1,43 +1,46 @@
 <template>
   <v-container>
-    <v-card :loading="loading" class="mx-auto my-12 detail" max-width="374">
+    <v-card class="mx-auto my-12 detail">
       <v-card-title
-        >Bulbasaur
+        >{{ pokemonName }}
         <h2>No. °001</h2></v-card-title
       >
-      <div class="detail__backgroundImg">
-        <v-img
-          src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
-        ></v-img>
-      </div>
-
-      <v-card-text>
-        <p>
-          Prefiere las cosas calientes. Dicen que cuando llueve le sale vapor de
-          la punta de cola.
-        </p>
-        <v-row no-gutters align="center" class="detail__version">
-          <h5>Versiones:</h5>
-          <div class="detail__version__select">
-            <v-img
-              max-width="25px"
-              contain
-              src="@/assets/icons/pokemon-one.png"
-            ></v-img>
-          </div>
+      <v-row no-gutters>
+        <div class="detail__backgroundImg">
           <v-img
-            max-width="26.5px"
-            contain
-            src="@/assets/icons/pokemon-two.png"
+            src="https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png"
           ></v-img>
-        </v-row>
-      </v-card-text>
-      <card-detail />
-      <v-card-text>
-        <h5 class="my-4">Tipo</h5>
-        <chips-custom name="Planta" color="#9dcb4f" textColor="#000000" />
-        <chips-custom name="Veneno" color="#b97fc9" textColor="#ffffff" />
-      </v-card-text>
+        </div>
+        <v-col>
+          <v-card-text>
+            <p>
+              Prefiere las cosas calientes. Dicen que cuando llueve le sale
+              vapor de la punta de cola.
+            </p>
+            <v-row no-gutters align="center" class="detail__version">
+              <h5>Versiones:</h5>
+              <div class="detail__version__select">
+                <v-img
+                  max-width="25px"
+                  contain
+                  src="@/assets/icons/pokemon-one.png"
+                ></v-img>
+              </div>
+              <v-img
+                max-width="26.5px"
+                contain
+                src="@/assets/icons/pokemon-two.png"
+              ></v-img>
+            </v-row>
+          </v-card-text>
+          <card-detail />
+          <v-card-text>
+            <h5 class="my-4">Tipo</h5>
+            <chips-custom name="Planta" color="#9dcb4f" textColor="#000000" />
+            <chips-custom name="Veneno" color="#b97fc9" textColor="#ffffff" />
+          </v-card-text>
+        </v-col>
+      </v-row>
     </v-card>
   </v-container>
 </template>
@@ -49,6 +52,9 @@ export default {
   name: "DetailPokemons",
   data() {
     return {};
+  },
+  props: {
+    pokemonName: String,
   },
   components: { ChipsCustom, CardDetail },
 };
